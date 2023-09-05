bf0699e94325dbc55f5f82456955b13532ec6d25
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2023-09-05 10:40:36 +0100

    Add initial script and README
    This is version 0.1 as published on [Greasy Fork][1], only with Prettier
    style fixes and an added README
    
    [1]: https://greasyfork.org/en/scripts/474513-breathehr-leave-calculator-hours-and-minutes-to-days/code

---
+++ README.md
@@ -0,0 +1,15 @@
+# Breathe leave calculator: hours and minutes to days
+
+[Breathe][breathe] is a HR tool. It can be used to track annual leave
+allowances. When you work part-time or compressed hours, you might see your
+leave allowance in hours and minutes rather than days. This is for accuracy, but
+it's hard to read as a human.
+
+This adds a approximate conversion from hours and minutes to days. This sits
+below the hours and minutes figure. You can enter your daily hours in a text
+box and the day calculation will update. By default, it's set to 7h45m per day.
+
+This script is published on and installable from [Greasy Fork][greasy-fork-script-page]
+
+[breathe]: https://www.breathehr.com
+[greasy-fork-script-page]: https://greasyfork.org/en/scripts/474513-breathehr-leave-calculator-hours-and-minutes-to-days/code
---
+++ script.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         BreatheHR leave calculator: hours and minutes to days
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Show an estimation of available leave in days when it's calculated in hours
+// @author       yndajas (they)
+// @license      MIT
+// @match        https://hr.breathehr.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=breathehr.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const calculateDays = () => {
+    return hours / dailyHours;
+  };
+
+  const updateDailyHoursValue = (newDailyHours) => {
+    console.log("updateDailyHoursValue", newDailyHours);
+    dailyHours = parseFloat(newDailyHours, 10);
+    days = calculateDays();
+    updateDaysValue();
+  };
+
+  const updateDaysValue = () => {
+    daysElement.innerText = `~${days.toFixed(2)} days`;
+  };
+
+  const leaveMetricElementSelector =
+    "#tab-my-dashboard > div > div:nth-child(1) > div.card-body > div.big-metric";
+
+  const timeElement = document.querySelector(
+    `${leaveMetricElementSelector} > strong`
+  );
+  const hoursAndMinutes = timeElement.innerText
+    .split(" ")
+    .map((time) => time.substring(0, time.length - 1));
+  const hours = parseFloat(hoursAndMinutes[0], 10) + hoursAndMinutes[1] / 60;
+
+  let dailyHours = 7.75;
+  let days = calculateDays();
+
+  const leaveMetricElement = document.querySelector(leaveMetricElementSelector);
+
+  const arrowElement = document.createElement("span");
+  arrowElement.classList.add("metric-subheading");
+  arrowElement.innerText = "↓";
+  arrowElement.style.margin = "0.5rem 0";
+
+  const daysElement = document.createElement("span");
+  daysElement.classList.add("metric-subheading");
+  updateDaysValue();
+
+  const dailyHoursInputWrapper = document.createElement("div");
+  dailyHoursInputWrapper.style.display = "flex";
+  dailyHoursInputWrapper.style.justifyContent = "center";
+
+  const dailyHoursInputLabel = document.createElement("label");
+  dailyHoursInputLabel.classList.add("metric-subheading");
+  dailyHoursInputLabel.for = "dailyHours";
+  dailyHoursInputLabel.innerText = "Hour per day:";
+  dailyHoursInputLabel.style.marginBottom = "0";
+  dailyHoursInputLabel.style.marginRight = "0.5rem";
+
+  const dailyHoursInput = document.createElement("input");
+  dailyHoursInput.classList.add("metric-subheading");
+  dailyHoursInput.name = "dailyHours";
+  dailyHoursInput.style.width = "3rem";
+  dailyHoursInput.value = dailyHours;
+  dailyHoursInput.addEventListener("keyup", (e) => {
+    const newDailyHours = e.target.value;
+    console.log("e.target.value:", newDailyHours);
+    updateDailyHoursValue(newDailyHours);
+  });
+
+  dailyHoursInputWrapper.append(dailyHoursInputLabel, dailyHoursInput);
+
+  leaveMetricElement.append(arrowElement, daysElement, dailyHoursInputWrapper);
+})();
