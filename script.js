63424671414075b3f89a02effe92f32f15500cb6
Ynda Jas <yndajas@gmail.com>
2024-11-19 17:13:49 +0000

    Update for new layout (November 2024)
    The BreatheHR dashboard layout has changed quite substantially. This
    updates the script to work with the new layout. This required updating
    the selectors, waiting for dependent content to load (the new dashboard
    seems to use hydration - the content isn't necessarily present on page
    load), and updating the layout/styling to fit better with the new
    BreatheHR dashboard styling
    
    This includes a major version bump given it's made for a completely new
    layout and won't work with the old one. The script has been working fine
    for many people for quite some time, so I think it's fine to push it to
    v1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Breathe leave calculator: hours and minutes to days
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      1.0.0
 // @description  Show an estimation of available leave in days when it's calculated in hours
 // @author       yndajas (they)
 // @license      MIT
@@ -13,71 +13,97 @@
 (function () {
   "use strict";
 
-  const calculateDays = () => {
-    return hours / dailyHours;
-  };
+  const leaveCardBodyElementSelector =
+    "[data-element-id='my-leave-widget'] > .card__content > .card__body-top";
 
-  const updateDailyHoursValue = (newDailyHours) => {
-    console.log("updateDailyHoursValue", newDailyHours);
-    dailyHours = parseFloat(newDailyHours, 10);
-    days = calculateDays();
+  const timeElement = () =>
+    document.querySelector(`${leaveCardBodyElementSelector} h4`);
+
+  const render = () => {
+    if (!timeElement()) {
+      return setTimeout(render, 1000);
+    }
+
+    const calculateDays = () => {
+      return hours / dailyHours;
+    };
+
+    const updateDailyHoursValue = (newDailyHours) => {
+      console.log("updateDailyHoursValue", newDailyHours);
+      dailyHours = parseFloat(newDailyHours, 10);
+      days = calculateDays();
+      updateDaysValue();
+    };
+
+    const updateDaysValue = () => {
+      daysElement.innerText = `~${days.toFixed(2)} days`;
+    };
+
+    const hoursAndMinutes = timeElement()
+      .innerText.replace(" left", "")
+      .split(" ")
+      .map((time) => time.substring(0, time.length - 1));
+
+    const hours =
+      parseFloat(hoursAndMinutes[0], 10) +
+      (hoursAndMinutes[1] ? hoursAndMinutes[1] / 60 : 0);
+
+    let dailyHours = 7.75;
+    let days = calculateDays();
+
+    const leaveCardBodyElement = document.querySelector(
+      leaveCardBodyElementSelector
+    );
+
+    const bodyColour = "#212529";
+    const bodyFontClasses = [
+      "bdds-text",
+      "bdds-text--body-base",
+      "bdds-text--style-normal",
+      "bdds-text--weight-regular",
+    ];
+
+    const containerElement = document.createElement("div");
+    containerElement.style.alignItems = "center";
+    containerElement.style.display = "flex";
+    containerElement.style.justifyContent = "space-between";
+    containerElement.style.marginTop = "0.5rem";
+
+    const daysElement = document.createElement("p");
+    daysElement.classList.add(...bodyFontClasses);
+    daysElement.style.color = bodyColour;
+    daysElement.style.marginBottom = "0";
     updateDaysValue();
+
+    const dailyHoursInputContainerElement = document.createElement("div");
+    dailyHoursInputContainerElement.style.color = bodyColour;
+    dailyHoursInputContainerElement.classList.add(...bodyFontClasses);
+
+    const dailyHoursInputLabel = document.createElement("label");
+    dailyHoursInputLabel.setAttribute("for", "dailyHours");
+    dailyHoursInputLabel.innerText = "Hours per day:";
+    dailyHoursInputLabel.style.marginBottom = "0";
+    dailyHoursInputLabel.style.marginRight = "0.5rem";
+
+    const dailyHoursInput = document.createElement("input");
+    dailyHoursInput.id = "dailyHours";
+    dailyHoursInput.style.fontSize = "1rem";
+    dailyHoursInput.style.marginBottom = "0";
+    dailyHoursInput.style.width = "3rem";
+    dailyHoursInput.value = dailyHours;
+    dailyHoursInput.addEventListener("keyup", (e) => {
+      const newDailyHours = e.target.value;
+      console.log("e.target.value:", newDailyHours);
+      updateDailyHoursValue(newDailyHours);
+    });
+
+    dailyHoursInputContainerElement.append(
+      dailyHoursInputLabel,
+      dailyHoursInput
+    );
+    containerElement.append(daysElement, dailyHoursInputContainerElement);
+    leaveCardBodyElement.append(containerElement);
   };
 
-  const updateDaysValue = () => {
-    daysElement.innerText = `~${days.toFixed(2)} days`;
-  };
-
-  const leaveMetricElementSelector =
-    "#tab-my-dashboard > div > div:nth-child(1) > div.card-body > div.big-metric";
-
-  const timeElement = document.querySelector(
-    `${leaveMetricElementSelector} > strong`
-  );
-  const hoursAndMinutes = timeElement.innerText
-    .split(" ")
-    .map((time) => time.substring(0, time.length - 1));
-  const hours =
-    parseFloat(hoursAndMinutes[0], 10) +
-    (hoursAndMinutes[1] ? hoursAndMinutes[1] / 60 : 0);
-
-  let dailyHours = 7.75;
-  let days = calculateDays();
-
-  const leaveMetricElement = document.querySelector(leaveMetricElementSelector);
-
-  const arrowElement = document.createElement("span");
-  arrowElement.classList.add("metric-subheading");
-  arrowElement.innerText = "↓";
-  arrowElement.style.margin = "0.5rem 0";
-
-  const daysElement = document.createElement("span");
-  daysElement.classList.add("metric-subheading");
-  updateDaysValue();
-
-  const dailyHoursInputWrapper = document.createElement("div");
-  dailyHoursInputWrapper.style.display = "flex";
-  dailyHoursInputWrapper.style.justifyContent = "center";
-
-  const dailyHoursInputLabel = document.createElement("label");
-  dailyHoursInputLabel.classList.add("metric-subheading");
-  dailyHoursInputLabel.for = "dailyHours";
-  dailyHoursInputLabel.innerText = "Hours per day:";
-  dailyHoursInputLabel.style.marginBottom = "0";
-  dailyHoursInputLabel.style.marginRight = "0.5rem";
-
-  const dailyHoursInput = document.createElement("input");
-  dailyHoursInput.classList.add("metric-subheading");
-  dailyHoursInput.name = "dailyHours";
-  dailyHoursInput.style.width = "3rem";
-  dailyHoursInput.value = dailyHours;
-  dailyHoursInput.addEventListener("keyup", (e) => {
-    const newDailyHours = e.target.value;
-    console.log("e.target.value:", newDailyHours);
-    updateDailyHoursValue(newDailyHours);
-  });
-
-  dailyHoursInputWrapper.append(dailyHoursInputLabel, dailyHoursInput);
-
-  leaveMetricElement.append(arrowElement, daysElement, dailyHoursInputWrapper);
+  render();
 })();
